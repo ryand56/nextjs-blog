7b3c1da66520a6ccbdfd5cd56e4f244c498805d4
Ryan Omasta <ryand@emeraldsys.xyz>
2023-01-04 22:13:59 -0700

    Faster way to get profile picture

--- components/common/Bio/Bio.js
+++ components/common/Bio/Bio.js
@@ -1,27 +1,23 @@
-import { useState, useEffect } from "react";
 import clsx from "clsx";
 
 import { Image } from "../Image";
 
-import { getSiteMetaData, getGithubProfile } from "@utils/helpers";
+import { getSiteMetaData } from "@utils/helpers";
 
 export function Bio({ className }) {
   const { author, social } = getSiteMetaData();
-  const [avatarUrl, setAvatarUrl] = useState("");
-
-  useEffect(() => getGithubProfile().then(url => setAvatarUrl(url)), []);
 
   return (
     <div className={clsx("flex items-center space-x-3", className)}>
       <div className="flex-shrink-0 mb-0 overflow-hidden rounded-full w-14 h-14">
-        {avatarUrl && <Image
+        <Image
           width={56}
           height={56}
-          src={avatarUrl}
+          src={`https://github.com/${social.github}.png`}
           alt="Profile"
           placeholder="blur"
-          blurDataURL="https://avatars.githubusercontent.com/u/10137?v=4"
-        />}
+          blurDataURL="https://github.com/ghost.png"
+        />
       </div>
 
       <p className="text-base leading-7">
